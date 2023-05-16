4b3c8dbd688d5e3291ebd87d8f7e2deb07320768
RobsterBNT <ROBBIE.BAKER22@bathspa.ac.uk>
2023-05-16 15:20:21 +0100

    Strategy Planner
    - Made start on canvas implementation
    - Remove Reviews Page
    - Add Planner page
    - Add button placeholders
    - Buttons require images and hover anim

--- script.js
+++ script.js
@@ -22,3 +22,66 @@ function darkModeFunc() {
     darkMode = false;
   } 
 }
+
+
+const canvas = document.getElementById("plannerCanvas");
+// then the Canvas Context - this lets us draw
+const ctx = canvas.getContext("2d");
+
+var background = new Image();
+background.src = "assets/Border_Map.webp";
+
+// Make sure the image is loaded first otherwise nothing will draw.
+background.onload = function(){
+    ctx.drawImage(background,0,0);   
+}
+
+//var canvas = document.createElement('canvas');
+//document.body.appendChild(canvas);
+
+// some hotfixes... ( ≖_≖)
+document.body.style.margin = 0;
+//canvas.style.position = 'fixed';
+
+// get canvas 2D context and set him correct size
+//var ctx = canvas.getContext('2d');
+//resize();
+
+// last known position
+var pos = { x: 0, y: 0 };
+
+window.addEventListener('resize', resize);
+document.addEventListener('mousemove', draw);
+document.addEventListener('mousedown', setPosition);
+document.addEventListener('mouseenter', setPosition);
+
+// new position from mouse event
+function setPosition(e) {
+  pos.x = e.clientX-60;
+  pos.y = e.clientY-155;
+}
+
+// resize canvas
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+function draw(e) {
+  // mouse left button must be pressed
+  if (e.buttons !== 1) return;
+
+  ctx.beginPath(); // begin
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = '#ff0000';
+
+  ctx.moveTo(pos.x, pos.y); // from
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to
+
+  ctx.stroke(); // draw it!
+}
+
+//https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
