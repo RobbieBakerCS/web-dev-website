895f65344f8adfe393b9b7af52a2209700161733
RobsterBNT <ROBBIE.BAKER22@bathspa.ac.uk>
2023-05-16 13:31:37 +0100

    Dark Mode
    - Start Dark Mode Implementation
    - Requires Javascript + CSS to change all elements

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+}, false);
+
+
+const bodyElement = document.getElementById("bodyID");
+const mainElement = document.getElementById("mainID");
+const darkModeBtn = document.getElementById("darkModeButton")
+const darkModeImg = document.getElementById("darkModeImg")
+let darkMode = false;
+
+function darkModeFunc() {
+  if (!darkMode) {
+    bodyElement.classList.toggle("darkMode");
+    mainElement.classList.toggle("darkMode");
+    darkModeImg.src = "/assets/SunImg.webp";
+    darkMode = true;
+  } else {
+    bodyElement.classList.toggle("darkMode");
+    mainElement.classList.toggle("darkMode");
+    darkModeImg.src = "/assets/MoonImg.webp";
+    darkMode = false;
+  } 
+}
