5a8c2716c9f44e7cae9b4dba4eaebfa4870d07a9
RobsterBNT <ROBBIE.BAKER22@bathspa.ac.uk>
2023-05-29 16:57:02 +0100

    Final Update
    - Comments added
    - Final CSS formatting tweaks
    - Game title image resize
    - 2x Banner image resize

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const darkModeImg = document.getElementById("darkModeImg")
 const darkModeTest = document.getElementsByClassName("darkModeTest")
 let darkMode = false;
 
+//Dark Mode function, grabs ID's and toggles CSS class change. Switches button image also
 function darkModeFunc() {
   if (!darkMode) {
     bodyElement.classList.toggle("darkMode");
@@ -24,32 +25,23 @@ function darkModeFunc() {
   } 
 }
 
-
+//Fetches empty canvas from planner.html
 const canvas = document.getElementById("plannerCanvas");
-// then the Canvas Context - this lets us draw
-//const ctx = canvas.getContext("2d");
-
+//Creates background variable and assigns transparent image
 var background = new Image();
 background.src = "assets/Transparent_Image.png";
 
-// Make sure the image is loaded first otherwise nothing will draw.
+//Draws transparent image onto background
 background.onload = function(){
     ctx.drawImage(background,0,0);
 }
 
-//var canvas = document.createElement('canvas');
-//document.body.appendChild(canvas);
-
-// some hotfixes... ( ≖_≖)
 document.body.style.margin = 0;
-//canvas.style.position = 'fixed';
-
-// get canvas 2D context and set him correct size
 var ctx = canvas.getContext('2d');
+//Calls resize function that sets image to correct size
 resize()
 
 
-// last known position
 var pos = { x: 0, y: 0 };
 
 window.addEventListener('resize', resize);
@@ -57,38 +49,40 @@ document.addEventListener('mousemove', draw);
 document.addEventListener('mousedown', setPosition);
 document.addEventListener('mouseenter', setPosition);
 
-// new position from mouse event
+//Fetches mouse position on page, edits made to adjust for drawing being off mouse position
 function setPosition(e) {
-  pos.x = e.clientX-65;
+  pos.x = e.clientX-62;
   pos.y = e.clientY-185;
 }
 
-// resize canvas
 function resize() {
   ctx.canvas.width = window.innerWidth-80;
   ctx.canvas.height = window.innerHeight-75;
 }
 
-
+//Sets brush stroke colour
 ctx.strokeStyle = '#ff0000';
-background.src = "assets/Border_Map.png";
+//Replaces background variable source and draws onto canvas
+background.src = "assets/Border_Map.webp";
 ctx.drawImage(background,0,0);
+
+//Function to handle drawing and brush format
 function draw(e) {
-  // mouse left button must be pressed
   if (e.buttons !== 1) return;
-
-  ctx.beginPath(); // begin
-
+  //Drawing path begins on click
+  ctx.beginPath();
+  //Sets brush size and line cap
   ctx.lineWidth = 5;
   ctx.lineCap = 'round';
 
-  ctx.moveTo(pos.x, pos.y); // from
+  ctx.moveTo(pos.x, pos.y);
   setPosition(e);
-  ctx.lineTo(pos.x, pos.y); // to
-
-  ctx.stroke(); // draw it!
+  ctx.lineTo(pos.x, pos.y);
+  //Creates stroke
+  ctx.stroke();
 }
 
+//Collect button elements from planner.html
 brush1Button = document.getElementById("brush1");
 brush2Button = document.getElementById("brush2");
 brush3Button = document.getElementById("brush3");
@@ -97,6 +91,8 @@ map1Button = document.getElementById("map1");
 map2Button = document.getElementById("map2");
 map3Button = document.getElementById("map3");
 
+//Function to handle brush switch, resets stroke colour,
+//adds border to selected brush and removes from others
 function brush1Switch() {
   ctx.strokeStyle = '#ff0000';
   brush1Button.style.border = "2px solid #ff0000";
@@ -118,15 +114,17 @@ function brush3Switch() {
   brush3Button.style.border = "2px solid #002cbd";
 }
 
+//Fetches clear canvas button and clears canvas, and redraws background
 clearCanvasButton = document.getElementById("clearCanvas");
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background,0,0);
-  clearCanvasButton.style.transform = 'rotate(360deg)';
 }
 
+//Function to handle map switch, redefines background source,
+//draw background to canvas, adds border to selected map and removes from others
 function map1Switch() {
-  background.src = "assets/Border_Map.png";
+  background.src = "assets/Border_Map.webp";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background,0,0);
   resize()
@@ -136,7 +134,7 @@ function map1Switch() {
 }
 
 function map2Switch() {
-  background.src = "assets/Chalet_Map.png";
+  background.src = "assets/Chalet_Map.webp";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background,0,0);
   map1Button.style.border = "0px";
@@ -145,7 +143,7 @@ function map2Switch() {
 }
 
 function map3Switch() {
-  background.src = "assets/House_Map.png";
+  background.src = "assets/House_Map.webp";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(background,0,0);
   map1Button.style.border = "0px";
@@ -153,4 +151,4 @@ function map3Switch() {
   map3Button.style.border = "2px solid #ffffff";
 }
 
-//https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
+//(Contributors, 2010)
